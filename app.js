186689cdbf649e402099de15921cc11aa134da88
Mikhail Koryakin <sdrr87@gmail.com>
2023-07-19 18:42:19 +0300

    feat: url not found

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const helmet = require('helmet');
 
+const { NOT_FOUND } = require('./utils/constants')
+
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
@@ -24,6 +26,8 @@ app.use((req, _res, next) => {
 
 app.use('/cards', require('./routes/cards'));
 app.use('/users', require('./routes/users'));
+
+app.use('*', (_req, res) => res.status(NOT_FOUND).send({ message: 'Страница не найдена' }));
 
 app.listen(PORT, () => {
   console.log(`App слушает ${PORT}`);
