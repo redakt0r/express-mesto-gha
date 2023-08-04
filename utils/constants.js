ed01b45fb25510e8f1ccb3104dd5a8c67b778e8f
Mikhail Koryakin <sdrr87@gmail.com>
2023-08-04 20:32:53 +0300

    feat: change port

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-escape */
 const STATUS_OK = 201;
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const SECRET_KEY = 'most-secret-key';
 
